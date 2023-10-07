6a94036e433ae52d4c799b58c7924e3511b749c6
ZLozZ <ironman242k2@gmail.com>
2023-10-07 14:33:50 +0700

    Add layout media and bookmarks

---
+++ app/src/main/java/com/example/learnandroid/BookMarks.kt
@@ -0,0 +1,11 @@
+package com.example.learnandroid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BookMarks : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_book_marks)
+    }
+}
