f1c73f84b5e21e42c43a398d4eb1ea19853383e3
ZLozZ <ironman242k2@gmail.com>
2023-10-24 01:16:45 +0700

    Add function login

--- app/src/main/java/com/example/learnandroid/MainActivity.kt
+++ app/src/main/java/com/example/learnandroid/MainActivity.kt
@@ -1,11 +1,33 @@
 package com.example.learnandroid
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.learnandroid.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    val login = Login()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        initControls()
+        initEvents()
+    }
+
+    private fun initControls() {
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.frLayout, login)
+            commit()
+        }
+    }
+
+    private fun initEvents() {
+
     }
 }
