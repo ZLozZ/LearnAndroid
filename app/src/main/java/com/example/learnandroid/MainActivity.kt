0a2c9df3161540c28c6e1cbc826dd20abaa0f908
ZLozZ <ironman242k2@gmail.com>
2023-10-29 19:52:06 +0700

    add b1 sensors

--- app/src/main/java/com/example/learnandroid/MainActivity.kt
+++ app/src/main/java/com/example/learnandroid/MainActivity.kt
@@ -1,11 +1,100 @@
 package com.example.learnandroid
 
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
+import com.example.learnandroid.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), SensorEventListener {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var sensorManager: SensorManager
+    private var proximitySensor : Sensor? = null
+    private var lightSensor: Sensor? = null
+    private var magneticSensor: Sensor? = null
+    private var tempSensor: Sensor? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+
+        setUpSensor()
+    }
+
+    private fun setUpSensor() {
+        sensorManager = getSystemService(SENSOR_SERVICE) as SensorManager
+        sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)?.also { accelerometer ->
+            sensorManager.registerListener(
+                this,
+                accelerometer,
+                SensorManager.SENSOR_DELAY_FASTEST,
+                SensorManager.SENSOR_DELAY_FASTEST
+            )
+        }
+        lightSensor = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT)
+        if (lightSensor != null) {
+            sensorManager.registerListener(this, lightSensor, SensorManager.SENSOR_DELAY_NORMAL)
+        } else {
+            binding.txtLight.text = "Light Sensor not available on this device"
+        }
+
+        proximitySensor = sensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY)
+        if (proximitySensor != null) {
+            sensorManager.registerListener(this, proximitySensor, SensorManager.SENSOR_DELAY_NORMAL)
+        } else {
+            binding.txtProximity.text = "Proximity Sensor not available on this device"
+        }
+
+        magneticSensor = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD_UNCALIBRATED)
+        if (magneticSensor != null) {
+            sensorManager.registerListener(this, magneticSensor, SensorManager.SENSOR_DELAY_NORMAL)
+        } else {
+            binding.txtMagnetic.text = "Step Sensor not available on this device"
+        }
+
+    }
+
+    override fun onSensorChanged(event: SensorEvent?) {
+        if (event?.sensor?.type == Sensor.TYPE_ACCELEROMETER) {
+            val sides = event.values[0]
+            val upDown = event.values[1]
+            binding.editSensorAccelerometer.setText("up/down ${upDown.toInt()}\nleft/right ${sides.toInt()}")
+        }
+        if (event?.sensor?.type == Sensor.TYPE_LIGHT) {
+            val light1 = event.values[0]
+
+            binding.txtLight.text = "Value Light sensor: $light1"
+        }
+
+        if (event?.sensor?.type == Sensor.TYPE_PROXIMITY) {
+            val proximity = event.values[0]
+
+            binding.txtProximity.text = "Value proximity sensor: $proximity"
+        }
+
+        if (event?.sensor?.type == Sensor.TYPE_MAGNETIC_FIELD_UNCALIBRATED) {
+            val magnetic = event.values[0]
+
+            binding.txtMagnetic.text = "Value Magnetic sensor: $magnetic"
+        }
+
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+    }
+
+    override fun onDestroy() {
+        sensorManager.unregisterListener(this)
+        super.onDestroy()
     }
 }
